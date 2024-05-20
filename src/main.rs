c292c9716ee8a35df9a7b006b74b81e8abf43424
Berkus Decker <berkus+github@metta.systems>
2024-05-20 13:13:19 +0300

    feat: ✨ Add REPL interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use {anyhow::{anyhow, Result}, argh::FromArgs};
+use {
+    anyhow::{anyhow, Error},
+    argh::FromArgs,
+    culpa::{throw, throws},
+    liso::{liso, Response},
+};
 
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -15,32 +20,52 @@ struct Args {
     script: Vec<String>,
 }
 
-fn main() -> Result<()> {
+#[throws]
+fn main() {
     let args: Args = argh::from_env();
 
     if args.version {
         println!("{} {}", APP_NAME, APP_VERSION);
-        return Ok(());
+        return;
     }
 
     if args.script.len() > 1 {
-        return Err(anyhow!("Usage: lochx [script file]"));
+        throw!(anyhow!("Usage: lochx [script file]"));
     }
 
     if args.script.len() == 1 {
         run_script(&args.script[0])?;
     } else {
-        run_repl();
+        run_repl()?;
     }
-
-    Ok(())
 }
 
-fn run_repl() {}
+#[throws]
+fn run_repl() {
+    let mut io = liso::InputOutput::new();
+    io.prompt(liso!(fg = green, bold, "> ", reset), true, false);
+    loop {
+        match io.read_blocking() {
+            Response::Input(line) => {
+                io.echoln(liso!(fg = green, dim, "> ", fg = none, &line));
+                run(line.as_str())?
+            }
+            Response::Discarded(line) => {
+                io.echoln(liso!(bold + dim, "X ", -bold, line));
+            }
+            Response::Dead => break,
+            Response::Quit => break,
+            Response::Finish => break,
+            _ => {}
+        }
+    }
+}
 
-fn run_script(script: &str) -> Result<()> {
+#[throws]
+fn run_script(script: &str) {
     let contents = std::fs::read_to_string(script)?;
-    run(&contents)
+    run(&contents)?
 }
 
-fn run(_text: &str) -> Result<()> { Ok(()) }
+#[throws]
+fn run(_text: &str) {}
